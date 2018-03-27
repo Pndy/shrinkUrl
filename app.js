6da03d6fa91df101701e26ba98760283bc272e3d
Pndy <pandastyl10@gmail.com>
2018-03-28 00:39:19 +0200

    Adding SSL and starting database schemas

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express')
+// Dependencies
+var express = require('express'),
+  https = require("https"),
+  fs = require('fs')
 
+// Express setup
 var app = express()
 
+//SSL Path
+var options = {
+  cert: fs.readFileSync('./config/ssl/fullchain.pem'),
+  key: fs.readFileSync('./config/ssl/privkey.pem')
+}
+
+//Routes
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(3000, () => {
+//Server listeners
+var http = app.listen(3000, () => {
   console.log("Listening on port 3000")
+})
+
+var server = https.createServer(options, app).listen(3001, () => {
+  console.log("Listening on port 3001 for https")
 })
---
+++ models/Users.model.js
@@ -0,0 +1,27 @@
+
+//Dependencies
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+//Schema rules
+var UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+// Export Schema
+module.exports = UserSchema;
